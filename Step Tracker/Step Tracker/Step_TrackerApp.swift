057e47094f5f5f9d22d38a63dc8de65241cd55c8
Steven Prichard <spprichard20@gmail.com>
2024-05-05 08:24:50 -0500

    Created Xcode Project
    Init project creation

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Steven Prichard on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
